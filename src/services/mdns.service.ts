cadf26c6f71fece7ef242fe98ce369b4b79acd01
idrimi <gerber@byrds.ch>
2026-02-04 22:05:08 +0100

    revert service

--- src/services/mdns.service.ts
+++ src/services/mdns.service.ts
@@ -1,79 +1,51 @@
 import { Bonjour } from 'bonjour-service';
 import os from 'os';
-import path from 'path';
-import fs from 'fs';
-
-// Helper to read package.json
-const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf-8'));
 
 export class MdnsService {
-    private bonjour: Bonjour;
-    private service: any;
+  private bonjour: Bonjour;
+  private service: any;
 
-    constructor() {
-        this.bonjour = new Bonjour();
-    }
+  constructor() {
+    this.bonjour = new Bonjour();
+  }
 
-    /**
-     * Get the MAC address of the first non-internal network interface
-     */
-    private getMacAddress(): string {
-        const interfaces = os.networkInterfaces();
-        for (const name of Object.keys(interfaces)) {
-            const iface = interfaces[name];
-            // Skip internal (loopback) interfaces
-            if (!iface) continue;
+  /**
+   * Start advertising the service via mDNS
+   */
+  async start(): Promise<void> {
+    const hostname = os.hostname();
+    console.log(`Starting mDNS advertisement for _beatnik._tcp on port 3000 (Host: ${hostname})`);
 
-            for (const alias of iface) {
-                if (!alias.internal && alias.mac !== '00:00:00:00:00:00') {
-                    return alias.mac;
-                }
-            }
-        }
-        return '00:00:00:00:00:00';
-    }
+    this.service = this.bonjour.publish({
+      name: `Beatnik Hardware API - ${hostname}`,
+      type: 'beatnik',
+      port: 3000,
+      protocol: 'tcp',
+      txt: {
+        version: '0.4.0'
+      }
+    });
 
-    /**
-     * Start advertising the service via mDNS
-     */
-    async start(): Promise<void> {
-        const hostname = os.hostname();
-        const mac = this.getMacAddress();
-        const version = packageJson.version;
+    this.service.on('error', (error: Error) => {
+      console.error('mDNS advertisement error:', error);
+    });
+  }
 
-        console.log(`Starting mDNS advertisement: http._tcp, Port: 3000, Host: ${hostname}, MAC: ${mac}, v${version}`);
-
-        this.service = this.bonjour.publish({
-            name: `Beatnik Hardware API - ${hostname}`,
-            type: 'http',
-            port: 3000,
-            protocol: 'tcp',
-            txt: {
-                version,
-                mac
-            }
+  /**
+   * Stop advertising the service
+   */
+  async stop(): Promise<void> {
+    if (this.service) {
+      console.log('Stopping mDNS advertisement...');
+      return new Promise((resolve) => {
+        this.service.stop(() => {
+          console.log('mDNS advertisement stopped');
+          this.bonjour.destroy();
+          resolve();
         });
-
-        this.service.on('error', (error: Error) => {
-            console.error('mDNS advertisement error:', error);
-        });
+      });
     }
-
-    /**
-     * Stop advertising the service
-     */
-    async stop(): Promise<void> {
-        if (this.service) {
-            console.log('Stopping mDNS advertisement...');
-            return new Promise((resolve) => {
-                this.service.stop(() => {
-                    console.log('mDNS advertisement stopped');
-                    this.bonjour.destroy();
-                    resolve();
-                });
-            });
-        }
-    }
+  }
 }
 
 export const mdnsService = new MdnsService();
